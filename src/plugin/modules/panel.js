812b084917504cf86adfa897812c8cf6f8a164f9
Erik Pearson <eapearson@lbl.gov>
2015-11-05 15:11:07 -0800

    Initial commit of this new plugin

---
+++ src/plugin/modules/panel.js
@@ -0,0 +1,33 @@
+/*global define*/
+/*jslint white: true, browser: true*/
+define([
+    
+], function () {
+    'use strict';
+    
+    function factory(config) {
+        var root, container;
+        function attach(node) {
+            root = node;
+            container = node.appendChild(document.createElement('div'));
+        }
+        function run(params) {
+            container.innerHTML = 'Hello, I\'m a Panel';
+        }
+        function detach() {
+            root.removeChild(container);
+        }
+        
+        return {
+            attach: attach,
+            run: run,
+            detach: detach
+        };
+    }
+    
+    return {
+        make: function (config) {
+            return factory(config);
+        }
+    };
+});
