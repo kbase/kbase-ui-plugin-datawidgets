0e939e5d64d939cbf5eed346a3c276a73bbeb11a
Erik Pearson <eapearson@lbl.gov>
2015-11-05 23:54:14 -0800

    added widgetset, and completed methods.

--- src/plugin/modules/panel.js
+++ src/plugin/modules/panel.js
@@ -1,35 +1,50 @@
 /*global define*/
 /*jslint white: true, browser: true*/
-define([    
-], function () {
+define([
+    'kb_common_widgetSet'
+], function (WidgetSet) {
     'use strict';
     
     function factory(config) {
-        var root, container;
+        var root, container, runtime = config.runtime,
+            widgetSet = WidgetSet.make({runtime: runtime});
+        
+        function init(config) {
+            return widgetSet.init(config);
+        }
+        
         function attach(node) {
             root = node;
             container = node.appendChild(document.createElement('div'));
+            return widgetSet.attach(container);
         }
         function start(params) {
-            
+            return widgetSet.start(params);
         }
         function run(params) {
             container.innerHTML = 'Hello, I\'m a Panel';
+            return widgetSet.run(params);
+        }
+        function stop() {
+            return widgetSet.stop();
         }
         function detach() {
             root.removeChild(container);
+            return widgetSet.detach();
         }
-        function stop() {
-            
+        function destroy() {
+            return widgetSet.destroy();
         }
         
-        return {
+        return Object.freeze({
+            init: init,
             attach: attach,
             start: start,
             run: run,
             stop: stop,
-            detach: detach
-        };
+            detach: detach,
+            destroy: destroy
+        });
     }
     
     return {
